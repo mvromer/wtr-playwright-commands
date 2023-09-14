a9c819b082d35fa8e4043450f6e0ef184c681738
Michael Romer <mvromer@gmail.com>
2023-09-14 00:16:49 -0500

    Add comments, fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { executeServerCommand } from '@web/test-runner-commands';
 
+/**
+ * Await the returned Promise to cause the current test to wait for Playwright to receive a network
+ * idle load status before resuming.
+ */
 export function waitForNetworkIdle() {
   return executeServerCommand('wait-for-network-idle');
 }
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,22 +1,26 @@
 import type { TestRunnerPlugin } from '@web/test-runner';
 import type { PlaywrightLauncher } from '@web/test-runner-playwright';
 
+/**
+ * Registers a new Web Test Runner command that allows a test to wait for Playwright to receive a
+ * network idle load status before resuming.
+ */
 export function waitForNetworkIdlePlugin(): TestRunnerPlugin {
   return {
     name: 'wait-for-network-idle-command',
 
-    async executeCommand({ command, payload, session }) {
+    async executeCommand({ command, session }) {
       if (command === 'wait-for-network-idle') {
         if (session.browser.type === 'playwright') {
           const page = (session.browser as PlaywrightLauncher).getPage(
-            session.id
+            session.id,
           );
           await page.waitForLoadState('networkidle');
           return true;
         }
 
         throw new Error(
-          `Cannot wait for idle load state in browser ${session.browser.type}`
+          `Cannot wait for network idle load state in browser ${session.browser.type}`,
         );
       }
     },
